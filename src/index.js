aec628d8beec2e6129c62b467d21a6f3efb89a56
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-04-12 19:41:00 +0430

    Add routes in index file

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import { router } from './routes/routes';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+  <RouterProvider router={router} />,
 );
 
