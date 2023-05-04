a1e32cd6a0f8a3468b4f840d4ab8c518e5523de9
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-05-04 15:14:18 +0430

    Edit routes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,10 +3,18 @@ import {
     createBrowserRouter,
 } from "react-router-dom";
 import App from "../App";
+import HomePage from "../pages/Home";
+import Layout from "../components/layout/layout";
 
 export const router = createBrowserRouter([
     {
         path: "/",
-        element: <App />,
+        element: <Layout />,
+        children: [
+            {
+                path: "/",
+                element: <HomePage/>
+            }
+        ]
     },
 ]);
