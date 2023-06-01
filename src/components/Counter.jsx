41f6b50eccd21d14bb6c741648a5f8fd46c78d13
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-06-01 13:08:05 +0430

    Subject: Counter, Description: Add buttons and handlers, Date: 6/1/2023

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,9 +1,37 @@
-const Counter = () => {
-    return ( 
+import { useState } from "react";
+
+const Counter = ({ initialState }) => {
+    const [counter, setCounter] = useState(initialState)
+
+    const incrementHandler = () => {
+
+    }
+
+    const descrementHandler = () => {
+
+    }
+
+    const resetHandler = () => {
+
+    }
+
+    const revertHandler = () => {
+
+    }
+    
+    return (
         <div>
-            
+            <h2 className="">
+                Counter: <b>{counter}</b>
+            </h2>
+            <div>
+                <button onClick={incrementHandler}>Increment</button>
+                <button onClick={descrementHandler}>Decrement</button>
+                <button onClick={resetHandler}>Reset</button>
+                <button onClick={revertHandler}>Revert</button>
+            </div>
         </div>
-     );
+    );
 }
- 
+
 export default Counter;
