fb2593595d312d7efac526f043799057de62e136
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-06-02 11:01:43 +0430

    Subject: UnitTest, Description: Add test for decrement button, Date: 6/2/2023

--- src/components/Counter.test.js
+++ src/components/Counter.test.js
@@ -8,11 +8,19 @@ describe(Counter, () => {
         expect(countValue).toEqual(0);
     })
 
-    it("count should increment by 1 if the decrement button is clicked", () => {
+    it("count should increment by 1 if the increment button is clicked", () => {
         const {getByTestId, getByRole} = render(<Counter initialState={0} />);
         const incrementButton = getByRole("button", {name: 'Increment'})
         expect(Number(getByTestId("count").textContent)).toEqual(0);
         fireEvent.click(incrementButton)
         expect(Number(getByTestId("count").textContent)).toEqual(1)
+    })
+
+    it("count should decrement by 1 decrement button is clicked", () => {
+        const {getByTestId, getByRole} = render(<Counter initialState={0} />)
+        const decrementButton = getByRole("button", {name: "Decrement"})
+        expect(Number(getByTestId('count').textContent)).toEqual(0)
+        fireEvent.click(decrementButton)
+        expect(Number(getByTestId('count').textContent)).toEqual(-1)
     })
 })
