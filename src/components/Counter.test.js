861333c31aa4dde44d58aa26670bcbb562fa3aa0
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-06-02 11:05:22 +0430

    Subject: UnitTest, Description: Add test for restart button, Date: 6/2/2023

--- src/components/Counter.test.js
+++ src/components/Counter.test.js
@@ -9,18 +9,26 @@ describe(Counter, () => {
     })
 
     it("count should increment by 1 if the increment button is clicked", () => {
-        const {getByTestId, getByRole} = render(<Counter initialState={0} />);
-        const incrementButton = getByRole("button", {name: 'Increment'})
+        const { getByTestId, getByRole } = render(<Counter initialState={0} />);
+        const incrementButton = getByRole("button", { name: 'Increment' })
         expect(Number(getByTestId("count").textContent)).toEqual(0);
         fireEvent.click(incrementButton)
         expect(Number(getByTestId("count").textContent)).toEqual(1)
     })
 
     it("count should decrement by 1 decrement button is clicked", () => {
-        const {getByTestId, getByRole} = render(<Counter initialState={0} />)
-        const decrementButton = getByRole("button", {name: "Decrement"})
+        const { getByTestId, getByRole } = render(<Counter initialState={0} />)
+        const decrementButton = getByRole("button", { name: "Decrement" })
         expect(Number(getByTestId('count').textContent)).toEqual(0)
         fireEvent.click(decrementButton)
         expect(Number(getByTestId('count').textContent)).toEqual(-1)
+    })
+
+    it("count should be 0 if the restart button is clicked", () => {
+        const { getByTestId, getByRole } = render(<Counter initialState={50} />)
+        const restartButton = getByRole("button", {name: "Reset"})
+        expect(Number(getByTestId('count').textContent)).toEqual(50)
+        fireEvent.click(restartButton)
+        expect(Number(getByTestId('count').textContent)).toEqual(0)
     })
 })
